cb20ed441297a79c15530d8c077866455ba1834c
emreerkus <emre.erkus@ozu.edu.tr>
2023-12-20 05:49:05 +0300

    adding pacman class and its move skills

---
+++ src/Entity.java
@@ -0,0 +1,9 @@
+import java.awt.image.BufferedImage;
+
+public class Entity {
+    public int x, y;
+    public int speed;
+    public int size;
+    public BufferedImage pacman_img;
+    public String direction;
+}
---
+++ src/Pacman.java
@@ -0,0 +1,75 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
+
+public class Pacman extends Entity {
+    GamePanel gamePanel;
+    KeyHandler keyHandler;
+    public int score;
+
+    public Pacman(GamePanel gamePanel, KeyHandler keyHandler){
+        this.gamePanel = gamePanel;
+        this.keyHandler = keyHandler;
+        setDefaultPosition();
+        getPlayerImage();
+    }
+
+    public void setDefaultPosition(){
+        x = 240;
+        y = 48;
+        speed = 2;
+        size = 48;
+        direction = "right";
+        score = 0;
+    }
+
+    public void getPlayerImage(){
+        try {
+            pacman_img = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/images/pacman.png")));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void move(){
+        switch(direction){
+            case "up":
+                y -= speed;
+                break;
+            case "down":
+                y += speed;
+                break;
+            case "right":
+                x += speed;
+                break;
+            case "left":
+                x -= speed;
+                break;
+            default:
+                break;
+        }
+    }
+    public void update(){
+        if(keyHandler.upPressed){
+            direction = "up";
+        }
+        else if(keyHandler.downPressed){
+            direction = "down";
+        }
+        else if(keyHandler.rightPressed){
+            direction = "right";
+        }
+        else if(keyHandler.leftPressed){
+            direction = "left";
+        }
+        move();
+    }
+
+    public void draw(Graphics2D g2){
+        BufferedImage image = null;
+
+        g2.drawImage(pacman_img, x, y, gamePanel.tileSize, gamePanel.tileSize, null);
+    }
+}
