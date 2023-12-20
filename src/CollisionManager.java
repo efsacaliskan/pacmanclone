4df5b374405edd02f472b129389c7a2af3995d3b
emreerkus <emre.erkus@ozu.edu.tr>
2023-12-20 06:27:08 +0300

    adding collision check with ghost function to collision manager

--- src/CollisionManager.java
+++ src/CollisionManager.java
@@ -47,4 +47,46 @@ public class CollisionManager {
         }
         return true;
     }
+
+    public boolean collisionWithGhost(int player_x, int player_y, int ghost_x, int ghost_y){
+
+        String[] rel_pos = {"", ""};
+
+        if(player_x > ghost_x){
+            rel_pos[0] = "right";
+        }else if(player_x < ghost_x){
+            rel_pos[0] = "left";
+        }else{
+            rel_pos[0] = "equal";
+        }
+
+        if(player_y > ghost_y){
+            rel_pos[1] = "bottom";
+        }else if(player_y < ghost_y){
+            rel_pos[1] = "above";
+        }else{
+            rel_pos[1] = "equal";
+        }
+
+
+        if(rel_pos[0].equals("right") && rel_pos[1].equals("bottom")) {
+            return player_x < ghost_x + gamePanel.tileSize && player_y < ghost_y + gamePanel.tileSize;
+        }else if(rel_pos[0].equals("right") && rel_pos[1].equals("above")){
+            return player_x < ghost_x + gamePanel.tileSize && player_y + gamePanel.tileSize > ghost_y;
+        }else if(rel_pos[0].equals("left") && rel_pos[1].equals("bottom")){
+            return player_x + gamePanel.tileSize > ghost_x && player_y < ghost_y + gamePanel.tileSize;
+        }else if(rel_pos[0].equals("left") && rel_pos[1].equals("above")){
+            return player_x + gamePanel.tileSize > ghost_x && player_y +gamePanel.tileSize > ghost_y;
+        }else if(rel_pos[0].equals("right")){
+            return player_x < ghost_x + gamePanel.tileSize;
+        }else if(rel_pos[0].equals("left")){
+            return player_x + gamePanel.tileSize > ghost_x;
+        }else if(rel_pos[1].equals("bottom")){
+            return player_y < ghost_y + gamePanel.tileSize;
+        }else if(rel_pos[1].equals("above")){
+            return player_y+gamePanel.tileSize > ghost_y;
+        }
+
+        return false;
+    }
 }
