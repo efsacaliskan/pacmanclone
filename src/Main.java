4a7625d392634e6b91fa4cb204e1d2da64d02947
emreerkus <emre.erkus@ozu.edu.tr>
2023-12-20 05:13:58 +0300

    creating java frame

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Pacman Clone");
     }
 }
