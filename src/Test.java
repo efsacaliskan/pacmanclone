f4ff267bf0864c94497276e9bae1fe1695bff2ca
talucil <talucil@hotmail.com>
2023-12-25 19:12:07 +0300

    Test Class added and will be modified.

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+public class Test{
+    // Testing for Pacman clone will be done in this class
+
+
+
+    // As first step, testing the Pacman class functions
+    // Creating pacman
+
+    GamePanel gp = new GamePanel();
+
+
+    KeyHandler kh = new KeyHandler();
+    Pacman p = new Pacman(gp,kh);
+
+    // Unit Testing approach will be applied by hand.
+
+
+
+
+
+}
